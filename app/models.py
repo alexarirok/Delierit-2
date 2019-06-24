6b58a4f0f3f09aa88a0ac28f807f00b256fb5d41
alexarirok <akorir233@gmail.com>
2019-06-24 14:59:08 +0300

    Refactor

--- app/models.py
+++ app/models.py
@@ -32,33 +32,33 @@ class Users(db.Model):
         return "<Users: {}>".format(self.name)
 
 class Parcels(db.Model):
-     __tablename__ = "parcels"
+    __tablename__ = "parcels"
 
-     id = db.Column(db.Integer, primary_key=True)
-     parcel_name = db.column(db.String(255))
-     pickup_destination = db.column(db.String(255))
-     delivery_destination = db.column(db.String(255))
-     parcel_number = db.column(db.Integer())
-     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-     date_modified = db.Column(
-     db.DateTime, default=db.func.current_timestamp(),
-     onupdate=db.func.current_timestamp())
+    id = db.Column(db.Integer, primary_key=True)
+    parcel_name = db.column(db.String(255))
+    pickup_destination = db.column(db.String(255))
+    delivery_destination = db.column(db.String(255))
+    parcel_number = db.column(db.Integer())
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+    db.DateTime, default=db.func.current_timestamp(),
+    onupdate=db.func.current_timestamp())
 
-     def __init__(self, name):
+    def __init__(self, name):
         # initialize with name.
         self.name = name
 
-     def save(self):
+    def save(self):
         db.session.add(self)
         db.session.commit()
 
-     @staticmethod
-     def get_all():
+    @staticmethod
+    def get_all():
         return Parcels.query.all()
 
-     def delete(self):
+    def delete(self):
         db.session.delete(self)
         db.session.commit()
 
-     def __repr__(self):
+    def __repr__(self):
         return "<Users: {}>".format(self.name)
